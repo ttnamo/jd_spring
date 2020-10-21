5006681aa83a128da6d94f46f3ec7670165606c4
tigi <ttnamo@gmail.com>
2020-10-21 06:29:45 -0700

    Spring loosly coupled

---
+++ Spring-00-LooslyCoupled/src/service/FullTimeMentor.java
@@ -0,0 +1,11 @@
+package service;
+
+import implementation.Mentor;
+
+public class FullTimeMentor implements Mentor {
+
+        public void createAccount() {
+            System.out.println("full time mentor account is created ");
+        }
+
+}
---
+++ Spring-00-LooslyCoupled/src/service/PartTimeMentor.java
@@ -0,0 +1,9 @@
+package service;
+
+import implementation.Mentor;
+
+public class PartTimeMentor implements Mentor {
+    public void createAccount(){
+        System.out.println("part time account is created ....");
+    }
+}
