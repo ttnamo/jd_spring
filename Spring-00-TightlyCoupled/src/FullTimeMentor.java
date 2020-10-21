a01574330841d96229eca5218f8b9b0b9fb02b0f
tigi <ttnamo@gmail.com>
2020-10-21 06:12:07 -0700

    Spring tightly coupled

---
+++ Spring-00-TightlyCoupled/src/FullTimeMentor.java
@@ -0,0 +1,6 @@
+public class FullTimeMentor {
+
+   public void  CreateAccount(){
+      System.out.println("Full time mentor account is created .....");
+   }
+}
---
+++ Spring-00-TightlyCoupled/src/PartTimeMentor.java
@@ -0,0 +1,5 @@
+public class PartTimeMentor {
+    public void createAccount(){
+        System.out.println("Part time mentor account is created ....");
+    }
+}
