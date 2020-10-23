36a0e271fca98ce833a512d708395db2c4d5ea52
tigi <ttnamo@gmail.com>
2020-10-23 08:04:27 -0700

    change of location for spring container with config.xml

---
+++ Spring-02-DI-ConstractorInjection/src/main/java/com/cybertek/Services/Java.java
@@ -0,0 +1,10 @@
+package com.cybertek.Services;
+
+import com.cybertek.interfaces.Course;
+
+public class Java implements Course {
+
+    public void getTeachingHours() {
+        System.out.println("weekly teaching hours 20");
+    }
+}
